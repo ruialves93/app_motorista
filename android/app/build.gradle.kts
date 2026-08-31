8d2fbf1c8549dd99d0aace91a6c056a483bfdba9
ruialves93 <ruialves093@gmail.com>
2026-08-31 21:03:27 +0100

    Update Flutter Android build for Java 25

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,15 +12,15 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
-        }
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
+}
 
     defaultConfig {
         applicationId = "com.ruibarata.cctvmotorista"
