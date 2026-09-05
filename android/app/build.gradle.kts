91a728eb6c067fc8344f8f22c9fe78c117c0d5f6
ruialves93 <ruialves093@gmail.com>
2026-09-05 23:54:40 +0100

    Update signing config with real credentials and track jks

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,15 +12,15 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-}
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
 
     defaultConfig {
         applicationId = "com.ruibarata.cctvmotorista"
@@ -31,22 +31,13 @@ kotlin {
     }
 
     signingConfigs {
-    create("release") {
-        val keystoreFile = System.getenv("KEYSTORE_FILE")
-
-        if (keystoreFile != null) {
-            storeFile = File(keystoreFile)
-            storePassword = System.getenv("KEYSTORE_PASSWORD")
-            keyAlias = System.getenv("KEY_ALIAS")
-            keyPassword = System.getenv("KEY_PASSWORD")
-        } else {
+        create("release") {
             storeFile = file("my-release-key.jks")
-            storePassword = "password123"
-            keyAlias = "my-key-alias"
-            keyPassword = "password123"
+            storePassword = "password123"  // Ex: "minhapassword123"
+            keyAlias = "my-key-alias"          // Ex: "key0"
+            keyPassword = "password123"    // Normalmente é igual à storePassword
         }
     }
-}
 
     buildTypes {
         release {
